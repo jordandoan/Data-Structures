66efbfe74c6a8c73a419a2d84ca9f1c93902b5fb
Jordan <jordandoan@hotmail.com>
2020-01-30 16:49:59 -0800

    General heap done

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -1,22 +1,58 @@
 class Heap:
-    def __init__(self, comparator):
+    def __init__(self, comparator=lambda x, y: x >= y):
         self.storage = []
         self.comparator = comparator
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self._bubble_up(self.get_size() - 1)
 
     def delete(self):
-        pass
+        self.storage[0], self.storage[-1] = self.storage[-1], self.storage[0]
+        val = self.storage.pop()
+        self._sift_down(0)
+        return val
 
     def get_priority(self):
-        pass
+        if self.storage:
+            return self.storage[0]
 
     def get_size(self):
-        pass
+        return len(self.storage)
 
     def _bubble_up(self, index):
-        pass
+        while self.hasParent(index) and self.comparator(self.storage[index], self.storage[self.getParent(index)]):
+            parent = self.getParent(index)
+            self.storage[parent], self.storage[index] = self.storage[index], self.storage[parent]
+            index = parent
 
     def _sift_down(self, index):
-        pass
+        while self.hasLeftChild(index):
+            left = self.getLeftChild(index)
+            larger_child = left
+            if self.hasRightChild(index):
+                right = self.getRightChild(index)
+                if self.comparator(self.storage[right], self.storage[left]):
+                    larger_child = right
+            if self.comparator(self.storage[index], self.storage[larger_child]):
+                break
+            self.storage[larger_child], self.storage[index] = self.storage[index], self.storage[larger_child]
+            index = larger_child
+
+    def hasParent(self, index):
+        return (index - 1) // 2 >= 0
+
+    def hasLeftChild(self, index):
+        return index * 2 + 1 < self.get_size()
+
+    def hasRightChild(self, index):
+        return (index + 1) * 2 < self.get_size()
+
+    def getLeftChild(self, index):
+        return index * 2 + 1
+
+    def getRightChild(self, index):
+        return (index + 1) * 2
+
+    def getParent(self, index):
+        return (index - 1) // 2
